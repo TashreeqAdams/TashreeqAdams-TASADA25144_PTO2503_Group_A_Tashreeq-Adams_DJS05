ec010e6150d7cc8047f76aa6536113b8b0ba858d
Tashreeq Adams <tashreeqadams8@gmail.com>
2026-01-31 20:55:01 +0200

    Added styling

--- src/components/Podcasts/PodcastDetail.jsx
+++ src/components/Podcasts/PodcastDetail.jsx
@@ -2,6 +2,7 @@ import { fetchSinglePodcast } from "../../api/fetchPodcasts";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import styles from "./PodcastDetail.module.css";
+import { formatDate } from "../../utils/formatDate";
 
 export default function PodcastDetail() {
   const { id } = useParams();
@@ -25,21 +26,71 @@ export default function PodcastDetail() {
   if (!podcast) return <p>Podcast not found</p>;
 
   return (
-    <div>
-      <h1>{podcast.title}</h1>
-      <img src={podcast.image} alt={podcast.title} />
-      <p>{podcast.description}</p>
-      <p>Last updated: {podcast.updated}</p>
-      <p>Genres: {podcast.genres?.join(", ")}</p>
-      <p>Total seasons: {podcast.seasons?.length}</p>
-      <p>
-        Total episodes:{""}
-        {podcast.seasons.reduce(
-          (total, season) => total + season.episodes.length,
-          0
-        )}
-      </p>
-      <h2>Current Season</h2>
+    <div className={styles.detailContainer}>
+      <div className={styles.detailInfo}>
+        <h1>{podcast.title}</h1>
+        <img
+          className={styles.detailImage}
+          src={podcast.image}
+          alt={podcast.title}
+        />
+        <p>{podcast.description}</p>
+        <p>Last updated: {formatDate(podcast.updated)}</p>
+        <p>Genres: {podcast.genres?.join(", ")}</p>
+        <p>Total seasons: {podcast.seasons?.length}</p>
+        <p>
+          Total episodes:{""}
+          {podcast.seasons.reduce(
+            (total, season) => total + season.episodes.length,
+            0
+          )}
+        </p>
+      </div>
+
+      <div className={styles.seasonSection}>
+        <h2>Current Season</h2>
+
+        <label className={styles.label}>
+          <select
+            className={styles.select}
+            value={selectedSeason?.season || ""}
+            onChange={(e) => {
+              const seasonNum = Number(e.target.value);
+              const season = podcast.seasons.find(
+                (s) => s.season === seasonNum
+              );
+              setSelectedSeason(season);
+            }}
+          >
+            {podcast.seasons.map((s) => (
+              <option key={s.season} value={s.season}>
+                {s.title}
+              </option>
+            ))}
+          </select>
+        </label>
+      </div>
+
+      {selectedSeason && (
+        <div className={styles.seasonBlock}>
+          <h2>{selectedSeason.title}</h2>
+
+          <ul className={styles.episodeList}>
+            {selectedSeason.episodes.map((ep) => (
+              <div className={styles.episodeTile}>
+                <img
+                  className={styles.episodeImage}
+                  src={podcast.image}
+                  alt={podcast.title}
+                />
+                <li className={styles.episodeList} key={ep.id}>
+                  {ep.title}
+                </li>
+              </div>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
